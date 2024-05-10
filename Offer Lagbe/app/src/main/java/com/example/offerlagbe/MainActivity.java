2b95d61aaaebff17a706301ed3fe1835bc6cd833
raduainrimon <163767983+raduainrimon@users.noreply.github.com>
2024-05-10 20:52:29 +0600

    RIH

--- Offer Lagbe/app/src/main/java/com/example/offerlagbe/MainActivity.java
+++ Offer Lagbe/app/src/main/java/com/example/offerlagbe/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.offerlagbe;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Toast;
-
 import com.example.offerlagbe.Fragment.AddFragment;
 import com.example.offerlagbe.Fragment.HomeFragment;
 import com.example.offerlagbe.Fragment.NotificationFragment;
@@ -13,6 +13,9 @@ import com.example.offerlagbe.Fragment.ProfileFragment;
 import com.example.offerlagbe.Fragment.SearchFragment;
 import com.example.offerlagbe.databinding.ActivityMainBinding;
 import com.iammert.library.readablebottombar.ReadableBottomBar;
+import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -23,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setSupportActionBar(binding.toolbar);
+
+        // Set the title with ctan color
+        SpannableString spannableString = new SpannableString("My Profile");
+        spannableString.setSpan(new ForegroundColorSpan(Color.CYAN), 0, spannableString.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        setTitle(spannableString);
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        binding.toolbar.setVisibility(View.GONE);
         transaction.replace(R.id.container,new HomeFragment());
         transaction.commit();
 
@@ -33,20 +44,25 @@ public class MainActivity extends AppCompatActivity {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 switch (i){
                     case 0:
+                        binding.toolbar.setVisibility(View.GONE);
                         Toast.makeText(MainActivity.this, "Home Selected", Toast.LENGTH_SHORT).show();
                         transaction.replace(R.id.container,new HomeFragment());
                         break;
                     case 1:
+                        binding.toolbar.setVisibility(View.GONE);
                         transaction.replace(R.id.container,new NotificationFragment());
                         break;
                     case 2:
+                        binding.toolbar.setVisibility(View.GONE);
                         transaction.replace(R.id.container,new AddFragment());
                         break;
                     case 3:
+                        binding.toolbar.setVisibility(View.GONE);
                         transaction.replace(R.id.container,new SearchFragment());
                         break;
-                        //Showing a notification on above of the page when we slide into it
+                    //Showing a notification on above of the page when we slide into it
                     case 4:
+                        binding.toolbar.setVisibility(View.VISIBLE);
                         Toast.makeText(MainActivity.this, "Profile Selected", Toast.LENGTH_SHORT).show();
                         transaction.replace(R.id.container,new ProfileFragment());
                         break;
@@ -54,5 +70,11 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item,menu);
+        return true;
     }
 }
